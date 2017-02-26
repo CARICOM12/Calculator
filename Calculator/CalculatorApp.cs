c7a7f3b963475d806f543f7934329948c06ffa69
CARICOM12 <0190a@ukr.net>
2017-02-26 19:58:14 +0200

    Calculator commence was created

---
+++ Calculator/CalculatorApp.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    internal class CalculatorApp
+    {
+        public CalculatorApp()
+        {
+
+        }
+        public int Compute(string expression)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorApp c = new CalculatorApp();
+            do
+            {
+                Console.WriteLine("Please enter an expression: ");
+                string expression = Console.ReadLine();
+                try
+                {
+                    int result = c.Compute(expression);
+                    Console.WriteLine("The result is: {0}", result);
+                }
+                catch
+                {
+                    Console.WriteLine("Error occured!");
+                }
+                Console.WriteLine("Press q to exit or any other key to continue");
+            }
+            while (Console.ReadKey().KeyChar != 'q') ;
+        }
+    }
+}
